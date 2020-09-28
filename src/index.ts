ebe3f31206a3e5fd832b8f25b35a872b456bdb33
Lucas Arusiewicz Lacerda <lucasarusiewicz@gmail.com>
2020-09-28 07:34:44 -0300

    Buscar dados dos argumentos

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
 import { Caso, Processador, Processo, Arvore } from './schema'
 
-const casoTeste = new Caso('casoTeste.txt')
-const processadores = Processador.pool(casoTeste.numProc)
-const arvore = new Arvore(false)
-Arvore.populaArvore(arvore, casoTeste)
+const args = process.argv
 
-let tarefasExecutadas = 0
-let tempoTotal = 0
+if (args.length !== 4) {
+  throw new Error('Argumentos inválidos de inicialização.\nExemplo: node . casoTeste.txt MIN')
+}
+
+if (args[3] !== 'MIN' && args[3] !== 'MAX') {
+  throw new Error('Política de escolha inválida.\nArgumentos válidos: MIN, MAX')
+}
+
+const caso = new Caso(args[2])
+const processadores = Processador.pool(caso.numProc)
+const arvore = new Arvore(args[3] === 'MIN')
+Arvore.populaArvore(arvore, caso)
+
+let ticksTotal = 0
 let tarefasPendentes = arvore.getPrioridade()
 while (tarefasPendentes.length || processadores.some(cpu => !cpu.estaLivre())) {
+  // Entrada de tarefas
   processadores.forEach(cpu => {
     if (cpu.estaLivre()) {
       const no = tarefasPendentes.shift()
@@ -19,14 +29,14 @@ while (tarefasPendentes.length || processadores.some(cpu => !cpu.estaLivre())) {
     }
   })
 
+  // Execução das tarefas
   processadores.forEach(cpu => {
     const processoFinalizado = cpu.tick()
     if (processoFinalizado) {
       tarefasPendentes = arvore.getPrioridade()
-      tarefasExecutadas++
     }
   })
-  tempoTotal++
+  ticksTotal++
 }
 
-console.log({ tarefasExecutadas, tempoTotal })
+console.log({ caso: caso.nome, proc: caso.numProc, tarefas: caso.tarefas.length, ticksTotal })
